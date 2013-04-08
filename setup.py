76012e5b36f3d08b9e45ca0c7ff875548048cec3
Christopher R Field <christopher.field@nrl.navy.mil>
2013-04-08 17:18:37 -0400

    Renamed files for consistency and improved documentation based on project name change.

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ except ImportError:
     from distutils.core import setup
 
 config = {
-          'name': 'dani',
+          'name': 'Zombie File',
           'author': 'Christopher R. Field',
-          'description': 'A desktop application for catalogin, organizing, and tagging research data files',
-          'url': '',
-          'download_url': '',
+          'description': 'A brain-dead simple system for organizing scientific data files',
+          'url': 'http://github.com/zombie-file/zombie-file.git',
+          'download_url': 'http://github.com/zombie-file/zombie-file.git',
           'author_email': 'cfield2@gmail.com',
           'version': '0.0.1',
           'install_requires': [],
