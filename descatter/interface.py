c45a6c87a5f1977fdec1da3d6ef5dfbf146029e8
Christopher R Field <christopher.field@nrl.navy.mil>
2013-04-12 18:30:47 -0400

    Improved CommandLine class in the interface module to easily test command line arguments and interface. Updated the command line interface to eliminate complicated subcommands. Added additional tests.

--- descatter/interface.py
+++ descatter/interface.py
@@ -2,25 +2,41 @@ import argparse
 import os 
 import constants
 import catalog
-import sys
 
 class CommandLine(object):
     
     def __init__(self):
         self.catalog = None
-        self.parser = argparse.ArgumentParser(description='An application for cataloging, organizing, and tagging files', prog='descatter')
-        subparsers = self.parser.add_subparsers(help="sub-command help")
-
-        parser_create = subparsers.add_parser(constants.CREATE_SUBCOMMAND_NAME, help="creates a catalog")
-        parser_create.add_argument('path', nargs='?', default=os.getcwd(), help="path of new catalog")
-        parser_create.set_defaults(func=self.create_catalog)
-
-    def parse(self):
-        args = self.parser.parse_args()
+        self.parser = argparse.ArgumentParser(description='An application for cataloging, organizing, and tagging files', 
+                                              prog='descatter')
         
-        if len(sys.argv) > 1:
-            args.func(args.path)
-            
-    def create_catalog(self, path):
-        self.catalog = catalog.Catalog()
-        self.catalog.create(path)
+        self.parser.add_argument(constants.CATALOG_ARGUMENT_SHORT_NAME, 
+                                 constants.CATALOG_ARGUMENT_LONG_NAME, 
+                                 nargs='?', 
+                                 default=os.getcwd(), 
+                                 help="specify the catalog")
+        
+        self.parser.add_argument(constants.ESTABLISH_ARGUMENT_SHORT_NAME, 
+                                 constants.ESTABLISH_ARGUMENT_LONG_NAME, 
+                                 help="establish or create a catalog",
+                                 action='store_true')
+        
+    def parse(self, test_args=None):
+        args = self.parser.parse_args(test_args)
+    
+        if args.establish:
+            self.establish_catalog(args.catalog)
+        elif args.checkin:
+            self.checkin_file(args.file_path)
+        else:
+            print(args)
+    
+    def establish_catalog(self, path):
+        self.catalog = catalog.Catalog(path)
+        self.catalog.establish()
+        
+    def destroy_catalog(self, path):
+        pass
+    
+    def checkin_file(self, path):
+        pass
--- tests/test_interface.py
+++ tests/test_interface.py
@@ -1,6 +1,5 @@
 from nose.tools import *
-
-import sys
+s
 import os
 import shutil
 import interface
@@ -11,22 +10,30 @@ class TestCommandLine(object):
     def __init__(self):
         self.test_path = os.path.join(os.getcwd(), constants.TESTS_FOLDER_NAME)
         self.test_catalog_path = os.path.join(self.test_path, constants.TEST_CATALOG_NAME)
-        self.calling_args = sys.argv
+        self.command_line_interface = interface.CommandLine()
     
     def setup(self):
         os.mkdir(self.test_catalog_path)
-        remove_args = sys.argv[:1]
-        sys.argv = remove_args
     
     def teardown(self):
+        shutil.rmtree(os.path.join(self.command_line_interface.catalog.path, constants.CONTENT_FOLDER_NAME))
+        shutil.rmtree(os.path.join(self.command_line_interface.catalog.path, constants.HOOKS_FOLDER_NAME))
+        shutil.rmtree(os.path.join(self.command_line_interface.catalog.path, constants.LOG_FOLDER_NAME))
+        shutil.rmtree(os.path.join(self.command_line_interface.catalog.path, constants.TEMPLATES_FOLDER_NAME))
         shutil.rmtree(self.test_catalog_path)
-        sys.argv = self.calling_args
     
-    def test_create(self):
-        sys.argv.append(constants.CREATE_SUBCOMMAND_NAME)
-        sys.argv.append(self.test_catalog_path)
+    def test_establish_without_path(self):
+        test_args = '-e'
+        self.command_line_interface.parse(test_args.split())
         
-        command_line_interface = interface.CommandLine()
-        command_line_interface.parse()
+        cwd = os.getcwd()
         
+        assert(self.command_line_interface.catalog.path == cwd, "The established catalog path is not the current working directory by default") 
+        assert(self.command_line_interface.catalog.name == constants.APPLICATION_NAME, "The catalog name is not 'descatter'")
+    
+    def test_establish_with_path(self):
+        test_args = '-c ' + self.test_catalog_path + ' -e' 
+        self.command_line_interface.parse(test_args.split())
         
+        assert(self.command_line_interface.catalog.path == self.test_catalog_path, "The established catalog path is not the 'Test_Catalog' directory in the 'tests' folder") 
+        assert(self.command_line_interface.catalog.name == constants.TEST_CATALOG_NAME, "The catalog name is not 'Test_Catalog'")
