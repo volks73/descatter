11ed3f0b6e223427ff6ad41a87894ebb89878024
volks73 <cfield2@gmail.com>
2013-07-19 16:50:57 -0400

    Reorganized code to separate out the filer component from the rest of the components that will eventually make up the descatter application. Also moved the schema code to the filer module and renamed it the 'director' code. The XML schema was changed to be more flexible and 'director' was more appropriate because it directs the filer where to place/copy source files. The 'Processor' class in the 'filer' module now handles obtaining the destination file path from the 'Director' and filing single files, list of files, and files in a folder. Rules were added to the XML schema to replace the 'destination' pattern that was implemented. This will provide for more complex conditions for evaluation in the future.

--- tests/test_filer.py
+++ tests/test_filer.py
@@ -3,19 +3,19 @@ import unittest
 import tempfile
 import shutil
 
-import descatter
+import filer
 
 TEST_FOLDER_NAME = 'tests'
 TEST_DATA_FOLDER_NAME = 'data'
-TEST_SCHEMA_FILE_PATH = os.path.join(os.getcwd(), TEST_FOLDER_NAME)
-TEST_SCHEMA_FILE_PATH = os.path.join(TEST_SCHEMA_FILE_PATH, TEST_DATA_FOLDER_NAME)
-TEST_SCHEMA_FILE_PATH = os.path.join(TEST_SCHEMA_FILE_PATH, "test_content_type_schema.xml")
+TEST_DIRECTIVE_FILE_PATH = os.path.join(os.getcwd(), TEST_FOLDER_NAME)
+TEST_DIRECTIVE_FILE_PATH = os.path.join(TEST_DIRECTIVE_FILE_PATH, TEST_DATA_FOLDER_NAME)
+TEST_DIRECTIVE_FILE_PATH = os.path.join(TEST_DIRECTIVE_FILE_PATH, "test_content_type_directive.xml")
 
 class TestFiler(unittest.TestCase):
     
     def setUp(self):
-        self.test_base_folder_path = tempfile.mkdtemp(suffix='', prefix='test_descatter_', dir=None)
-        test_file_handle, self.test_source_file_path = tempfile.mkstemp(suffix='.txt', prefix='test_descatter_', dir=None, text=False)
+        self.test_base_folder_path = tempfile.mkdtemp(suffix='', prefix='test_filer_', dir=None)
+        test_file_handle, self.test_source_file_path = tempfile.mkstemp(suffix='.txt', prefix='test_filer_', dir=None, text=False)
         test_file = os.fdopen(test_file_handle, 'w')
         test_file.close()
         
@@ -23,14 +23,8 @@ class TestFiler(unittest.TestCase):
         os.remove(self.test_source_file_path)
         shutil.rmtree(self.test_base_folder_path)
         
-    def test_file(self):
-        filer = descatter.Filer(self.test_base_folder_path, TEST_SCHEMA_FILE_PATH)
-        filer.file(self.test_source_file_path)
+    def test_file_single(self):
+        processor = filer.Processor(self.test_base_folder_path, TEST_DIRECTIVE_FILE_PATH)
+        processor.file(self.test_source_file_path)
         
         # TODO: Add check for file copied to appropriate destination
-    
-    def test_batch_file(self):
-        pass
-    
-    def test_recursive_file(self):
-        pass
